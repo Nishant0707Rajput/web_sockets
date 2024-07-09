5ba920271154675743895faf9a04be1107cc97a4
nishant.rajput <nishant.rajput@appventurez.com>
2024-07-09 11:08:21 +0530

    gitignore added

--- websocket_front_end/src/app/app.component.ts
+++ websocket_front_end/src/app/app.component.ts
@@ -1,36 +1,10 @@
 import { Component } from '@angular/core';
-import { WebSocketService } from './web-socket.service';
-import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'front-end';
-  socket: any;
-  messages: string[] = [];
-  private messageSubscription: Subscription | null = null;
-
-
-  constructor(private webSocketService:WebSocketService){
-
-  }
-  ngOnInit() {
-     this.messageSubscription = this.webSocketService.connect('ws://localhost:8080')
-      .subscribe((message: string) => {
-        console.log('Received message', message);
-        this.messages = [...this.messages, message];
-      });
-  }
-  sendMessageToServer(value: string) {
-    this.webSocketService.sendMessage(value);
-  }
-
-  ngOnDestroy() {
-    if (this.messageSubscription) {
-      this.messageSubscription.unsubscribe();
-    }
-  }
 }
